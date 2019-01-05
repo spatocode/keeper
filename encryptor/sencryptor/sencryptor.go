a709915f7a35548092163b541f88ae9f641b271b
Ekene Izukanne <ekeneizukanne@gmail.com>
2019-01-05 13:23:41 +0100

    create a symmetric encryption

---
+++ encryptor/sencryptor/sencryptor.go
@@ -0,0 +1,24 @@
+package sencryptor
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"log"
+)
+
+var IV = []byte("1234567812345678")
+
+func createCipher(key []byte) cipher.Block{
+	ciph, err := aes.NewCipher(key)
+	if err != nil {
+		log.Fatalf("Failed to create the AES cipher: %s", err)
+	}
+	return ciph
+}
+
+func Encrypt(file []byte, key []byte) []byte{
+	blockCipher := createCipher(key)
+	stream := cipher.NewCTR(blockCipher, IV)
+	stream.XORKeyStream(file, file)
+	return file
+}
